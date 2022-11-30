ee7de9712675de651eb52f149851650c8f500183
DaniStonks <200221067@estudantes.ips.pt>
2022-11-30 23:24:36 +0000

    l7

--- puzzle.lisp
+++ puzzle.lisp
@@ -21,7 +21,7 @@
   (second tabuleiro))
 
 (defun get-arco-na-posicao (pos-lista-arcos pos-arco tabuleiro)
-  (nth (1- pos-arco) (nth (1- pos-hori) tabuleiro)))
+  (nth (1- pos-arco) (nth (1- pos-lista-arcos) tabuleiro)))
 
 ;;substituir
 
@@ -31,4 +31,23 @@
         (t (cons (car lista) (substituir (1- indice) (cdr lista) x)))))
 
 (defun arco-na-posicao (pos-lista-arcos pos-arco lista-arcos &optional (x 1))
-  (cons (car lista-arcos) (cons (substituir pos-arco (nth pos-lista-arcos lista-arcos) x) (cdr lista-arcos))))
+  (cond ((= (1- pos-lista-arcos) 0) (cons (substituir pos-arco (car lista-arcos) x) (cdr lista-arcos)))
+        (t (cons (car lista-arcos) (arco-na-posicao (1- pos-lista-arcos) pos-arco (cdr lista-arcos) x)))))
+
+;;Operadores
+
+(defun arco-horizontal (pos-lista-arcos pos-arco tabuleiro &optional (x 1))
+  (let* ((arcos-hor (car tabuleiro))
+        (linhas (length arcos-hor))
+        (colunas (length (car arcos-hor))))
+    (cond ((or (> pos-lista-arcos linhas)(> pos-arco colunas)) NIL)
+          ((= 1 (get-arco-na-posicao pos-lista-arcos pos-arco arcos-hor)) NIL)
+          (t (cons (arco-na-posicao pos-lista-arcos pos-arco arcos-hor x) (cdr tabuleiro))))))
+
+(defun arco-vertical (pos-lista-arcos pos-arco tabuleiro &optional (x 1))
+  (let* ((arcos-ver (cadr tabuleiro))
+        (linhas (length arcos-ver))
+        (colunas (length (car arcos-ver))))
+    (cond ((or (> pos-lista-arcos linhas)(> pos-arco colunas)) NIL)
+          ((= 1 (get-arco-na-posicao pos-lista-arcos pos-arco arcos-ver)) NIL)
+          (t (cons (car tabuleiro)(arco-na-posicao pos-lista-arcos pos-arco arcos-ver x))))))
--- puzzle.lisp~
+++ puzzle.lisp~
@@ -1,5 +1,5 @@
 ;;;; puzzle.lisp
-;;;; Funções especificas ao dominio do jogo Dots and Boxes
+;;;; Funcoes especificas ao dominio do jogo Dots and Boxes
 ;;;; Autor: Daniel Baptista, Rafael Silva
 
 
@@ -7,10 +7,28 @@
 
 (defun tabuleiro-teste ()
   "Retorna um tabuleiro 3x3 (3 arcos na vertical por 3 arcos na horizontal)"
-	'(
-		((0 0 0) (0 0 1) (0 1 1) (0 0 1))
-		((0 0 0) (0 1 1) (1 0 1) (0 1 1))
-	)
-)
+  '(
+    ((0 0 0) (0 0 1) (0 1 1) (0 0 1))
+    ((0 0 0) (0 1 1) (1 0 1) (0 1 1))
+    ))
 
-;;; Exercicios
+;;Seletores
+
+(defun get-arcos-horizontais (tabuleiro)
+  (first tabuleiro))
+
+(defun get-arcos-verticais (tabuleiro)
+  (second tabuleiro))
+
+(defun get-arco-na-posicao (pos-lista-arcos pos-arco tabuleiro)
+  (nth (1- pos-arco) (nth (1- pos-hori) tabuleiro)))
+
+;;substituir
+
+(defun substituir (indice lista &optional (x 1))
+  (cond ((null lista) nil)
+        ((= (1- indice) 0) (cons x (substituir (1- indice) (cdr lista) x)))
+        (t (cons (car lista) (substituir (1- indice) (cdr lista) x)))))
+
+(defun arco-na-posicao (pos-lista-arcos pos-arco lista-arcos &optional (x 1))
+  (cons (car lista-arcos) (cons (substituir pos-arco (nth pos-lista-arcos lista-arcos) x) (cdr lista-arcos))))
